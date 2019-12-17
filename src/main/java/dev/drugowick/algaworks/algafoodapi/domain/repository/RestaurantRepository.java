cc6bee3f541981cf348e8070f479108a90434e68
Bruno Drugowick <bruno.drugowick@gmail.com>
2019-12-16 21:55:56 -0300

    Moves some JPQL to orm.xml file.
    This presents a third option to create your Repository queries, one where the query is externalized from the code to a configuration file.

--- src/main/java/dev/drugowick/algaworks/algafoodapi/domain/repository/RestaurantRepository.java
+++ src/main/java/dev/drugowick/algaworks/algafoodapi/domain/repository/RestaurantRepository.java
@@ -2,7 +2,6 @@ package dev.drugowick.algaworks.algafoodapi.domain.repository;
 
 import dev.drugowick.algaworks.algafoodapi.domain.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
@@ -10,13 +9,13 @@ import java.util.List;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
-    @Query("from Restaurant r join r.cuisine c where c.name like %:name%")
     List<Restaurant> byCuisineLike(@Param("name") String cuisine);
     // In Query Method would be: List<Restaurant> findAllByCuisineNameContaining(String cuisine);
+    // With annotation would be: //@Query("from Restaurant r join r.cuisine c where c.name like %:name%")
 
-    @Query("from Restaurant where deliveryFee >= :initial and deliveryFee <= :final")
+
     List<Restaurant> byDeliveryFee(@Param("initial") BigDecimal startingFee,
                                    @Param("final") BigDecimal endingFee);
     // In QueryMethod would be: List<Restaurant> findAllByDeliveryFeeBetween(BigDecimal startingFee, BigDecimal endingFee);
-
+    // With annotation would be: //@Query("from Restaurant where deliveryFee >= :initial and deliveryFee <= :final")
 }
