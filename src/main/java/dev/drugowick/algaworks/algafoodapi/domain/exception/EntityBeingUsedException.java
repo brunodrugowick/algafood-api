74439924392d3027b826e08650a9b7d3d434def6
Bruno Drugowick <bruno.drugowick@gmail.com>
2019-12-29 19:23:12 -0300

    Introduces Spring's ResponseStatusException class.
    ResponseStatusException class allows you throw an exception with a specific message and HTTP status code. A good fit for simple PoCs.
    With that, it's not necessary to handle HTTP codes on the service exception classes (ee how the exception classes are back to being basic), but you still are kind of limited.
    Let's follow along...

--- src/main/java/dev/drugowick/algaworks/algafoodapi/domain/exception/EntityBeingUsedException.java
+++ src/main/java/dev/drugowick/algaworks/algafoodapi/domain/exception/EntityBeingUsedException.java
@@ -1,9 +1,5 @@
 package dev.drugowick.algaworks.algafoodapi.domain.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.CONFLICT)
 public class EntityBeingUsedException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
