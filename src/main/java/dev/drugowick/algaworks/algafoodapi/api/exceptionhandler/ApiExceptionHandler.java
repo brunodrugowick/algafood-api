297bef6531b3bae3632b2947d582aa9ea732bb6a
Bruno Drugowick <bruno.drugowick@gmail.com>
2020-01-18 16:56:01 -0300

    Adds extension to the convenience class ResponseEntityExceptionHandler on the Controller Advice.
    With this, it's possible to remove handlers for specific exceptions from outside our code and only customize the handleExceptionInternal method.
    Also, the custom handlers can also use our implementation of handleExceptionInternal, providing a centralized way of defining response body for exceptions of the API.

--- src/main/java/dev/drugowick/algaworks/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/dev/drugowick/algaworks/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,60 +3,78 @@ package dev.drugowick.algaworks.algafoodapi.api.exceptionhandler;
 import dev.drugowick.algaworks.algafoodapi.domain.exception.EntityBeingUsedException;
 import dev.drugowick.algaworks.algafoodapi.domain.exception.EntityNotFoundException;
 import dev.drugowick.algaworks.algafoodapi.domain.exception.GenericBusinessException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<ApiError> handler(EntityNotFoundException exception) {
-        ApiError apiError = ApiError.builder()
-                .timestamp(LocalDateTime.now())
-                .message(exception.getMessage())
-                .build();
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiError);
+    public ResponseEntity<?> handler(EntityNotFoundException exception, WebRequest request) {
+        return handleExceptionInternal(
+                exception,
+                exception.getMessage(),
+                new HttpHeaders(),
+                HttpStatus.NOT_FOUND,
+                request
+        );
     }
 
     @ExceptionHandler(EntityBeingUsedException.class)
-    public ResponseEntity<ApiError> handler(EntityBeingUsedException exception) {
-        ApiError apiError = ApiError.builder()
-                .timestamp(LocalDateTime.now())
-                .message(exception.getMessage())
-                .build();
-
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(apiError);
+    public ResponseEntity<?> handler(EntityBeingUsedException exception, WebRequest request) {
+        return handleExceptionInternal(
+                exception,
+                exception.getMessage(),
+                new HttpHeaders(),
+                HttpStatus.CONFLICT,
+                request
+        );
     }
 
     @ExceptionHandler(GenericBusinessException.class)
-    public ResponseEntity<ApiError> handler(GenericBusinessException exception) {
-        ApiError apiError = ApiError.builder()
-                .timestamp(LocalDateTime.now())
-                .message(exception.getMessage())
-                .build();
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
+    public ResponseEntity<?> handler(GenericBusinessException exception, WebRequest request) {
+        return handleExceptionInternal(
+                exception,
+                exception.getMessage(),
+                new HttpHeaders(),
+                HttpStatus.BAD_REQUEST,
+                request
+        );
     }
 
     /**
-     * Example of handling exceptions of Spring (not your own code), in order to provide your custom model for other exceptions.
+     * Customizes Spring's handleExceptionInternal to create a default body for all possible exceptions already
+     * handled by ResponseEntityExceptionHandler class that we extend here.
      *
-     * @param exception HttpMediaTypeNotSupportedException
-     * @return The ApiError with Unsupported Media Type (415) HTTP status code.
+     * @param ex      The exception, that won't be touched here..
+     * @param body    The HTTP response body, that will be defined here as being or ApiError class.
+     * @param headers The HTTP response headers, that won't be touched here.
+     * @param status  The HTTP status, that won't be touched here.
+     * @param request The HTTP request, that won't be touched here.
+     * @return call Spring's handleExceptionInternal.
      */
-    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-    public ResponseEntity<?> handler(HttpMediaTypeNotSupportedException exception) {
-        ApiError apiError = ApiError.builder()
-                .timestamp(LocalDateTime.now())
-                .message(exception.getMessage())
-                .build();
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
 
-        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(apiError);
+        if (body == null) {
+            body = ApiError.builder()
+                    .timestamp(LocalDateTime.now())
+                    .message(status.getReasonPhrase())
+                    .build();
+        } else if (body instanceof String) {
+            body = ApiError.builder()
+                    .timestamp(LocalDateTime.now())
+                    .message((String) body)
+                    .build();
+        }
+
+        return super.handleExceptionInternal(ex, body, headers, status, request);
     }
 }
