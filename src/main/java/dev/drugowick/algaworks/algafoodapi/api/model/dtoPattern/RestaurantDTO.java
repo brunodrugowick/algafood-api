92d73174a3110b134ad9bca616e25a8727a50b96
Bruno Drugowick <bruno.drugowick@gmail.com>
2020-06-18 22:55:58 -0300

    Adds a working example of embedded property on the response.

--- src/main/java/dev/drugowick/algaworks/algafoodapi/api/model/dtoPattern/RestaurantDTO.java
+++ src/main/java/dev/drugowick/algaworks/algafoodapi/api/model/dtoPattern/RestaurantDTO.java
@@ -14,14 +14,16 @@ public enum RestaurantDTO {;
     private interface Id { @NotNull Long getId(); }
     private interface Name { @NotBlank String getName(); }
     private interface Delivery { @Multiple(number = 3) BigDecimal getDelivery(); }
+    private interface Cuisine { @NotBlank CuisineDTO.Response.Summary getCuisine(); }
     private interface Active { @NotNull Boolean getActive(); }
     private interface Opened { @NotNull Boolean getOpened(); }
 
     public enum Response {;
-        @Getter @Setter @NoArgsConstructor public static class Default implements Id, Name, Delivery, Active, Opened {
+        @Getter @Setter @NoArgsConstructor public static class Default implements Id, Name, Delivery, Cuisine, Active, Opened {
             Long id;
             String name;
             BigDecimal delivery;
+            CuisineDTO.Response.Summary cuisine;
             Boolean active;
             Boolean opened;
         }
