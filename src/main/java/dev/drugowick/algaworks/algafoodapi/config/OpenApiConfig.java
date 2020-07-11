ac941f92ba592f11e9d598a666e01eb0a23c4f1b
Bruno Drugowick <bruno.drugowick@gmail.com>
2020-07-11 00:28:13 -0300

    Adds global configuration of return codes for GET operations.

--- src/main/java/dev/drugowick/algaworks/algafoodapi/config/OpenApiConfig.java
+++ src/main/java/dev/drugowick/algaworks/algafoodapi/config/OpenApiConfig.java
@@ -3,16 +3,23 @@ package dev.drugowick.algaworks.algafoodapi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -26,8 +33,24 @@ public class OpenApiConfig {
                     .apis(RequestHandlerSelectors.basePackage("dev.drugowick"))
                     .paths(PathSelectors.any())
                     .build()
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.GET, globalGetReponseMessages())
                 .apiInfo(apiInfo())
                 .tags(new Tag("Cities", "Manages cities."));
+    }
+
+    private List<ResponseMessage> globalGetReponseMessages() {
+        return Arrays.asList(
+                new ResponseMessageBuilder()
+                    .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                    .message("Internal server error").build(),
+                new ResponseMessageBuilder()
+                    .code(HttpStatus.NOT_ACCEPTABLE.value())
+                    .message("There's no valid representation that the client accepts").build(),
+                new ResponseMessageBuilder()
+                    .code(HttpStatus.NOT_FOUND.value())
+                    .message("Resource not found").build()
+        );
     }
 
     private ApiInfo apiInfo() {
