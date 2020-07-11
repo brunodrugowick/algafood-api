771b3a5f1934b6cfa8b022015a14937a08a7f79a
Bruno Drugowick <bruno.drugowick@gmail.com>
2020-07-10 22:53:37 -0300

    Customizes info on the documentation page. Adds a link on the Demo homepage.

--- src/main/java/dev/drugowick/algaworks/algafoodapi/config/OpenApiConfig.java
+++ src/main/java/dev/drugowick/algaworks/algafoodapi/config/OpenApiConfig.java
@@ -2,8 +2,11 @@ package dev.drugowick.algaworks.algafoodapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,6 +21,16 @@ public class OpenApiConfig {
                 .select()
                     .apis(RequestHandlerSelectors.basePackage("dev.drugowick"))
                     .paths(PathSelectors.any())
+                    .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Drugo AlgafoodAPI")
+                .description("Algafood API is a REST API for a food delivery solution")
+                .version("1.0.0")
+                .contact(new Contact("Bruno Drugowick", "http://drugo.dev", "bruno.drugowick@gmail.com"))
                 .build();
     }
 }
