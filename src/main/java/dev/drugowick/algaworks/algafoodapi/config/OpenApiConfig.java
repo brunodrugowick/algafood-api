d5d0dc21caa64c2a1d4ed9e6e6ed38d1412a7fdc
Bruno Drugowick <bruno.drugowick@gmail.com>
2020-07-11 00:18:11 -0300

    Adds a dependency that is able to understand some of the BeanValidation annotations.

--- src/main/java/dev/drugowick/algaworks/algafoodapi/config/OpenApiConfig.java
+++ src/main/java/dev/drugowick/algaworks/algafoodapi/config/OpenApiConfig.java
@@ -2,6 +2,8 @@ package dev.drugowick.algaworks.algafoodapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,6 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
 public class OpenApiConfig {
 
     @Bean
